e2680cd1ce23fda60c5de11b6a8fa1a74a6d1fe3
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2023-04-09 00:00:10 +0300

    обновил

---
+++ interface.py
@@ -0,0 +1,39 @@
+import input
+import main
+
+power_on = True
+while power_on:
+    func_select = main.function_select()
+    if func_select == 1:
+        while True:
+            search_select = main.search_select()
+            if search_select == 1:
+                name = main.search()
+                print(name)
+                search_select = main.search_select()
+            if search_select == 2:
+                phone_number = main.search()
+                print(phone_number)
+                search_select = main.search_select()
+            if search_select == 3:
+                break
+    if func_select == 2:
+            while True:
+                redact = main.note_redact()
+                if redact == 1:
+                    note = main.new_note()
+                    print(f'Новая заметка {note} добавлена в список ')
+                    break
+                if redact == 2:
+                    func_select = main.function_select()
+                    break
+                else:
+                    print('Вы ввели недопустимое значение, попробуйте снова!')
+                    redact = main.notepad_redact()
+    if func_select == 3:
+       main.notepad_read()
+       print()
+    if func_select == 4:
+            print('Спасибо, что пользуетесь нашим приложением. До свидания ')
+            print()
+            power_on = False
